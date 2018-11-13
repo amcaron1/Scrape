9dd48641123d1fde648adb9efe0fe4e1fedc27a0
Andre <amcaron1@msn.com>
2018-11-13 14:53:24 -0600

    first push

---
+++ public/app.js
@@ -0,0 +1,118 @@
+// Scrapes the db for articles and displays them
+$(document).on("click", ".display_scrape", function() {
+  // Empties the current articles so that new ones are not appended on to the old ones
+  $("#articles").empty();
+
+  // Grabs the articles as a json
+  $.get("/scrape", function(data) {
+  
+    // For each one
+    for (var i = 0; i < data.length; i++) {
+
+      // Display the apropos information on the page
+      $("#articles").append("<div class='article'><a href=" + data[i].link + " target='_blank'>" + data[i].title + "</a><button type='button' class='save_article'>Save Article</button><br><p>" + data[i].summary + "</p></div>");
+    }
+  })
+})
+
+// Saves a selected article
+$(document).on("click", ".save_article", function() {
+  var title = $(this).siblings("a").text();
+  var link = $(this).siblings("a").attr("href");
+  var summary = $(this).siblings("p").text();
+
+  var data = {
+    title: title,
+    link: link,
+    summary: summary
+  };
+
+  $.post("/save_article/", data, function(response) {
+
+    // The user is notified if it is a duplicate
+    if (response == "duplicate") {
+      $("#message").text("This article was not saved.  It already resides in the database.");
+      $("#messageModal").modal("show");
+    }
+
+    // The user is notified if it is successfully savede 
+    else {
+      $("#message").text("This article was successfully saved in the database.");
+      $("#messageModal").modal("show");
+    }
+  })
+})
+
+// Displays saved articles
+$(document).on("click", ".display_saved", function() {
+
+  // Empties the current articles so that new ones are not appended on to the old ones
+  $("#articles").empty();
+
+  // Grabs the articles as a json
+  $.get("/saved_articles", function(data) {
+
+    // For each one
+    for (var i = 0; i < data.length; i++) {
+
+      // Display the apropos information on the page
+      // The id is hidden in an h6
+      $("#articles").append("<div class='article'><a href=" + data[i].link + " target='_blank' >" + data[i].title + "</a><button type='button' class='delete_article'>Delete Article</button><button type='button' class='display_notes'>Display Notes</button><br><p>" + data[i].summary + "</p><h6 hidden>" + data[i]._id + "</h6></div>");
+    }
+  })
+})
+    
+// Deletes a saved article
+$(document).on("click", ".delete_article", function() {
+  var this_id = $(this).siblings("h6").text();
+
+  // Removes the article from display
+  $(this).parent().remove();
+  $.get("/delete_article/" + this_id, function(response) {
+    console.log("response: " + response);
+  })
+})
+
+// Saves a new note
+$(document).on("click", ".save_note", function() {
+  var thisId = $("#article_id").text();
+  var note = $("#new_note").val();
+
+  var data = {
+    body: note
+  };
+  console.log("data:" + data);
+ 
+  $.post("/save_note/" + thisId, data, function(response) {
+
+      // Hide the modal
+      $("#notesModal").modal("hide");
+    });
+
+});
+
+// Displays the notes for an article
+$(document).on("click", ".display_notes", function() {
+
+  $("#article_id").text("");
+  $("#old_notes").empty();
+  $("#new_note").val("");
+
+  var this_id = $(this).siblings("h6").text();
+  $("#article_id").text(this_id);
+
+  $.get("/notes/" + this_id, function(data) {
+    if (data.note) {
+      for (var i = 0; i < data.note.length; i++){
+        $("#old_notes").append("<div class='note'><p>" + data.note[i].body + "</p><button type='button' class='delete_note'>Delete Note</button><br><h6 hidden>" + data[i]._id + "</h6></div>");
+
+      }
+    }
+  $("#notesModal").modal("show");
+  console.log("data: " + data);
+  })
+})
+
+
+     
+        
---
+++ server.js
@@ -0,0 +1,168 @@
+var express = require("express");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+
+// Our scraping tools
+// Axios is a promised-based http library, similar to jQuery's Ajax method
+// It works on the client and on the server
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+// Require all models
+var db = require("./models");
+
+var PORT = 3000;
+
+// Initialize Express
+var app = express();
+
+// Configure middleware
+
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Make public a static folder
+app.use(express.static("public"));
+
+// Connect to the Mongo DB
+mongoose.connect("mongodb://localhost/scrape", { useNewUrlParser: true });
+
+// Routes
+
+// Route for scraping the db for articles and sending them to the client
+app.get("/scrape", function(req, res) {
+
+  // First, we grab the body of the html with axios
+  axios.get("http://www.historytoday.com/latest/").then(function(response) {
+
+    // Then, we load that into cheerio and save it to $ for a shorthand selector
+    var $ = cheerio.load(response.data);
+
+    // Creates an array for storing the articles
+    var result_array = [];
+    
+    // Now, we grab every h3 that is a child of class field-name-title, and do the following:
+    $(".field-name-title h3").each(function(i, element) {
+
+      // Save an empty result object
+      var result = {};
+    
+      // Add the text, the href of every link, and an article summary, and save them as properties of the result object
+      result.title = $(this)
+        .children("a")
+        .text();
+      result.link = "http://www.historytoday.com" + $(this)
+        .children("a")
+        .attr("href");
+      result.summary = $(this).parent().parent().parent()
+        .siblings("div.field-name-field-summary").children().children().children("p").text();
+      result_array.push(result);
+      
+    });
+
+    // Return the articles to the client
+    res.json(result_array);
+  });
+});
+
+// Route for saving a new Article in the db
+app.post("/save_article/", function(req,res) {
+
+  // First checks to see if the article has already been saved
+  db.Article.findOne({"title": req.body.title})
+    .then(function(dbArticle) {
+      
+      // If it is a new article, it is saved
+      if (dbArticle == null) {
+        db.Article.create(req.body)
+          .then(function(dbArticle) {
+            res.json("saved");
+          })
+          .catch(function(err) {
+            // If an error occurred, log it
+            console.log(err);
+          })
+      }
+
+      // Else the client is notified that it is a duplicate
+      else {
+        res.json("duplicate");
+      }
+    })
+})
+
+// Route for deleting an Article from the db
+app.get("/delete_article/:id", function(req,res) {
+
+  db.Article.findOneAndDelete({_id: req.params.id})
+    .then(function(response) {
+      res.json("deleted")
+    })
+    .catch(function(err) {
+
+      // If an error occurred, log it
+      console.log(err);
+    })
+})
+
+// Route for getting all Articles from the db
+app.get("/saved_articles", function(req, res) {
+
+  // Grab every document in the Articles collection
+  db.Article.find({})
+    .then(function(dbArticle) {
+
+      // If we were able to successfully find Articles, send them back to the client
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
+
+// Route for creating a new note in the db
+app.post("/save_note/:id", function(req,res) {
+
+  db.Note.create(req.body)
+    .then(function(dbNote) {
+      // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
+      // { new: true } tells the query that we want it to return the updated Article -- it returns the original by default
+      // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
+      return db.Article.findOneAndUpdate({ _id: req.params.id }, { $push:{note: dbNote._id} }, { new: true });
+    })
+    .then(function(dbArticle) {
+      // If we were able to successfully update an Article, send it back to the client
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+
+  })
+
+app.get("/notes/:id", function(req,res) {
+
+  console.log("id: " + req.body.id);
+
+  db.Article.findOne({_id: req.params.id})
+    // ..and populate all of the notes associated with it
+    .populate("note")
+    .then(function(dbArticle) {
+      console.log("dbArticle: " + dbArticle);
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      // If an error occurred, log it
+      console.log(err);
+    })
+})
+
+// Start the server
+app.listen(PORT, function() {
+  console.log("App running on port " + PORT + "!");
+});
