50d38b755e3534d2a1158d3dc7f7d46e1457300d
Andre <amcaron1@msn.com>
2018-11-14 12:11:32 -0600

    heroku problems

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(express.static("public"));
 // Connect to the Mongo DB
 
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/scrape";
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+mongoose.connect(MONGODB_URI);
 
 // Routes
 
