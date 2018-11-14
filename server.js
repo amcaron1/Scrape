85f43d04f08dade014b8c6e99f5069f341686c4e
Andre <amcaron1@msn.com>
2018-11-14 09:59:13 -0600

    heroku

--- server.js
+++ server.js
@@ -27,7 +27,9 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/scrape", { useNewUrlParser: true });
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/scrape";
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 // Routes
 
