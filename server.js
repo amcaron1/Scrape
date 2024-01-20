b8499001d4e48b6506059060b262dd01fc2800cc
Andre Caron <amcaron1@gmail.com>
2024-01-20 12:45:05 -0600

    favicon

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ var cheerio = require("cheerio");
 // Require all models
 var db = require("./models");
 
-var PORT = process.env.PORT || 3000;git 
+var PORT = process.env.PORT || 3000;
 
 // Initialize Express
 var app = express();
